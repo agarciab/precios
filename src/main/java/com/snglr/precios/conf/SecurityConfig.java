40d289a4435f065924385d9947cd2893ef650931
Alexandre Garcia Boté <alex.garcia.bote@gmail.com>
2025-02-01 13:03:17 +0100

    chore: Estructura del proyecto y configuracion inicial

---
+++ src/main/java/com/snglr/precios/conf/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.snglr.precios.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/test/**", "/actuator/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger/**")
+						.permitAll().anyRequest().authenticated())
+				.csrf(AbstractHttpConfigurer::disable);
+		return http.build();
+	}
+}
---
+++ src/test/java/com/snglr/precios/PreciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snglr.precios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
